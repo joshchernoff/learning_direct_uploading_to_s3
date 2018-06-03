c14923081475f008f4bbfef4a179dd5f159fc3a7
Josh Chernoff <jchernoff@polymorphic.productions>
2018-06-03 11:35:35 -0700

    add deps

--- mix.exs
+++ mix.exs
@@ -6,9 +6,9 @@ defmodule LearningDirectUploadingToS3.Mixfile do
       app: :learning_direct_uploading_to_s3,
       version: "0.0.1",
       elixir: "~> 1.4",
-      elixirc_paths: elixirc_paths(Mix.env),
-      compilers: [:phoenix, :gettext] ++ Mix.compilers,
-      start_permanent: Mix.env == :prod,
+      elixirc_paths: elixirc_paths(Mix.env()),
+      compilers: [:phoenix, :gettext] ++ Mix.compilers(),
+      start_permanent: Mix.env() == :prod,
       aliases: aliases(),
       deps: deps()
     ]
@@ -26,7 +26,7 @@ defmodule LearningDirectUploadingToS3.Mixfile do
 
   # Specifies which paths to compile per environment.
   defp elixirc_paths(:test), do: ["lib", "test/support"]
-  defp elixirc_paths(_),     do: ["lib"]
+  defp elixirc_paths(_), do: ["lib"]
 
   # Specifies your project dependencies.
   #
@@ -40,7 +40,11 @@ defmodule LearningDirectUploadingToS3.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      {:ex_aws, "~> 2.0"},
+      {:poison, "~> 3.1"},
+      {:hackney, "~> 1.12"},
+      {:uuid, "~> 1.1"}
     ]
   end
 
@@ -54,7 +58,7 @@ defmodule LearningDirectUploadingToS3.Mixfile do
     [
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
-      "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+      test: ["ecto.create --quiet", "ecto.migrate", "test"]
     ]
   end
 end
